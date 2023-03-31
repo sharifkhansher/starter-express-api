b4f7205d674fdf63e02d69973283437e84382bf8
sharikhansher <sharifkhansherani2@gmail.com>
2023-03-31 21:49:26 +0530

    liveproject1

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(cors())
  app.use('/api/shopeen', require('./routes/sginuproutes'));
  app.use('/api/address', require('./routes/addressroutes'));
 app.use('/api/data',require('./routes/dataroutes'))
+app.use('/api/intiles',require('./routes/intilesroutes'))
 
 console.log("hii sharif khan sherani");
 
